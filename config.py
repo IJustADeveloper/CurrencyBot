d6168449222927bf24976431af49cced8342c139
IJustADeveloper <kopnov.nikita@gmail.com>
2022-11-03 22:15:10 +0300

    config clear

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "5757313852:AAGAuDwo-maawqvHfCX8V9qHaNhS51vzVRk"
-host = "redis-11676.c293.eu-central-1-1.ec2.cloud.redislabs.com"
-port = "11676"
-password = "6wdHV6YEjdTOTeQS0RsvfXEGegy3jNza"
+token = ""
+host = ""
+port = ""
+password = ""
